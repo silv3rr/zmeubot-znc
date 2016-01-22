d790984a86249e6daf764dcf628b16d69b08cd83
ZmEu <zmeu@foonet.ro>
2016-01-22 14:46:04 +0200

    Update zmeubot.cpp

--- zmeubot.cpp
+++ zmeubot.cpp
@@ -1,3 +1,17 @@
+/*
+The MIT License (MIT)
+Copyright (c) 2016 - zmeu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+Your donations pay for projects, staff, servers and protective infrastructure.
+BTC: 1zmeu5BeWBprWyPv5ntNZKR7uThXaG9ic
+*/
+
 #include "znc.h"
 #include "Chan.h"
 #include "User.h"
@@ -151,10 +165,6 @@ virtual void OnModCommand(const CString& sCommand) {
 		PutModule("ZmEu Bot v0.1 - running on " + Running + " v" + CZNC::Get().GetVersion());
 	} else if (sCommand.Equals("help")) {
 		PutModule("Contact me at a@foonet.org");
-		PutModule("---------------");
-		PutModule("Your donations pay for projects, staff, servers and protective infrastructure.");
-		PutModule("BTC: 1zmeu5BeWBprWyPv5ntNZKR7uThXaG9ic");
-		PutModule("---------------");
 	} else {
 		PutModule("Failed to run command: " + sCommand + ". -- For help try: help");
 	}
